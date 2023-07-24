e8fa4005747af187e935c33b813ab43d3f90e94e
ArletaJanysz <arleta.janysz@gmail.com>
2023-07-24 17:03:42 +0200

    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const { email, password } = loginForm.elements;
+
+	if (email.value === "" || password.value === "") {
+		alert("Uzupełnij wszystkie pola formularza!");
+		return;
+	}
+
+	const formDataObject = {
+		email: email.value,
+		password: password.value,
+	};
+
+	console.log(formDataObject);
+	loginForm.reset;
+});
