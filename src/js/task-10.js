fe2f42f88d2376ee43330d7f2f10134dc52640bf
ArletaJanysz <arleta.janysz@gmail.com>
2023-07-27 08:27:51 +0200

    commits

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,42 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+	const boxesDiv = document.getElementById("boxes");
+	boxesDiv.innerHTML = ""; // Clear previous boxes
+
+	let boxSize = 30;
+	for (let i = 0; i < amount; i++) {
+		const box = document.createElement("div");
+		box.style.width = boxSize + "px";
+		box.style.height = boxSize + "px";
+		box.style.backgroundColor = getRandomHexColor();
+		boxesDiv.appendChild(box);
+
+		boxSize += 10;
+	}
+}
+
+const destroyButton = document.querySelector("[data-destroy]");
+destroyButton.addEventListener("click", () => {
+	const boxesDiv = document.getElementById("boxes");
+	boxesDiv.innerHTML = "";
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const createButton = document.querySelector('[data-create]');
+  createButton.addEventListener('click', () => {
+    const amountInput = document.querySelector('input[type="number"]');
+    const amount = parseInt(amountInput.value);
+    if (isNaN(amount) || amount <= 0) {
+      alert('Please enter a valid number greater than 0.');
+      return;
+    }
+
+    createBoxes(amount);
+  });
+
+})
